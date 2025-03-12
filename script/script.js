cf63af64d2b1209909cf9c1c92a8f54a42336799
Raheel Rahat <raheelarfeen@example.com>
2025-03-12 23:13:41 +0600

    finished everything

--- script/script.js
+++ script/script.js
@@ -1,61 +1,162 @@
+const showLoader = () => {
+  document.getElementById("loader").classList.remove("hidden");
+  document.getElementById("video-container").classList.add("hidden");
+};
+
+const hideLoader = () => {
+  document.getElementById("loader").classList.add("hidden");
+  document.getElementById("video-container").classList.remove("hidden");
+};
+
+function removeActiveClass() {
+  const activeButtons = document.getElementsByClassName("active");
+  for (let btn of activeButtons) {
+    btn.classList.remove("active");
+  }
+}
+
 function loadCategories() {
-    fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
-    .then(res => res.json())
-    .then(data => displayCategories(data.categories))
+  fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+    .then((res) => res.json())
+    .then((data) => displayCategories(data.categories));
 }
 
-function displayCategories(categories){
-    const categoryContainer = document.getElementById('category-container')
-
-    for(let cat of categories){
-        const categoryBtn = document.createElement('div')
-        categoryBtn.innerHTML = `<button class="btn btn-sm bg-[#25252520] hover:bg-[#ff1f3d] hover:text-white">${cat.category}</button>`
-        categoryContainer.append(categoryBtn)
-    }
-}
-
-loadCategories()
-
-function loadVideos() {
-    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
-    .then(res => res.json())
-    .then(data => displayVideos(data.videos)) 
-}
-
-const displayVideos = (videos) =>{
-    const videoContainer = document.getElementById('video-container')
-
-    videos.forEach(video => {
-
-        const videoCard = document.createElement('div')
-        videoCard.innerHTML = `
-            <div class="card bg-base-100">
-                <figure class="relative">
-                    <img class="rounded-lg w-full h-[240px] object-cover" src="${video.thumbnail}" alt="Shoes" />
-                    <span class="absolute bottom-2 right-2 text-xs text-white bg-[#171717] rounded-md py-1 px-1.5">3hrs 56 min ago</span>
-                </figure>
-                <div class="card py-4 pr-2 flex">
-                    <div class="flex gap-4">
-                        <div class="profile">
-                            <div class="avatar">
-                                <div class="w-12 rounded-full">
-                                  <img src="${video.authors[0].profile_picture}" />
-                                </div>
-                            </div>
-                        </div>
-                        <div class="intro">
-                            <div>
-                                <h2 class="font-semibold text-xl">Midnight Serenade</h2>
-                                <p class="text-sm font-light flex items-center gap-2 text-gray-400">Awlad Hossain <img class="w-5" src="https://img.icons8.com/?size=100&id=QMxOVe0B9VzG&format=png&color=000000" alt=""></p>
-                                <span class="text-sm font-light text-gray-400">91K views</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        `
-        videoContainer.append(videoCard)
+function loadVideos(searchText = "") {
+  showLoader();
+  fetch(`https://openapi.programming-hero.com/api/phero-tube/videos?title=${searchText}`)
+    .then((response) => response.json())
+    .then((data) => {
+      removeActiveClass();
+      document.getElementById("btn-all").classList.add("active");
+      displayVideos(data.videos);
     });
 }
 
-loadVideos()
+const loadCategoryVideos = (id) => {
+  showLoader();
+  const url = `https://openapi.programming-hero.com/api/phero-tube/category/${id}`;
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      removeActiveClass();
+
+      const clickedButton = document.getElementById(`btn-${id}`);
+      clickedButton.classList.add("active");
+
+      displayVideos(data.category);
+    });
+};
+const loadVideoDetails = (videoId) => {
+  const url = `https://openapi.programming-hero.com/api/phero-tube/video/${videoId}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayVideoDetails(data.video));
+};
+
+const displayVideoDetails = (video) => {
+  document.getElementById("video_details").showModal();
+  const detailsContainer = document.getElementById("details-container");
+
+  detailsContainer.innerHTML = `
+    <div class="card bg-base-100 image-full shadow-sm">
+        <figure>
+            <img src="${video.thumbnail}"/>
+        </figure>
+        <div class="card-body">
+            <h2 class="card-title">Card Title</h2>
+            <p>${video.description}</p>
+            <div class="card-actions justify-end"></div>
+        </div>
+    </div>
+  `;
+};
+
+function displayCategories(categories) {
+  const categoryContainer = document.getElementById("category-container");
+
+  for (let cat of categories) {
+
+    const categoryDiv = document.createElement("div");
+
+    categoryDiv.innerHTML = `
+        <button id="btn-${cat.category_id}"  onclick="loadCategoryVideos(${cat.category_id})" class="btn btn-sm hover:bg-[#FF1F3D]  hover:text-white">${cat.category}</button>
+    `;
+
+    categoryContainer.append(categoryDiv);
+  }
+}
+
+const displayVideos = (videos) => {
+  const videoContainer = document.getElementById("video-container");
+
+  videoContainer.innerHTML = "";
+
+  if (videos.length == 0) {
+    videoContainer.innerHTML = `
+        <div class="py-20 col-span-full flex flex-col justify-center items-center text-center">
+            <img class="w-[150px]" src="./assets/Icon.png" alt="" />
+            <h2 class="text-2xl font-bold">
+            Oops!! Sorry, There is no content here
+            </h2>
+        </div>
+    `;
+    hideLoader();
+    return;
+  }
+
+  videos.forEach((video) => {
+
+    const videoCard = document.createElement("div");
+
+    videoCard.innerHTML = `
+     <div class="card bg-base-100">
+        <figure class="relative">
+          <img class="w-full h-[230px] object-cover" src="${video.thumbnail}" alt="Shoes" />
+          <span class="absolute bottom-2 right-2 text-sm rounded text-white bg-black px-2">3hrs 56 min ago</span>
+        </figure>
+
+        <div class="flex gap-3 px-0 py-5">
+          <div class="profile">
+            <div class="avatar">
+              <div class="ring-primary ring-offset-base-100 w-6 rounded-full ring ring-offset-2">
+                <img src="${video.authors[0].profile_picture}"/>
+              </div>
+            </div>
+          </div>
+
+          <div class="intro">
+            <h2 class="text-sm font-semibold">Midnight Serenade</h2>
+            <p class="text-sm text-gray-400 flex gap-1">
+             ${video.authors[0].profile_name}
+              ${
+                video.authors[0].verified == true
+                  ? `<img
+                class="w-5 h-5"
+                src="https://img.icons8.com/?size=96&id=98A4yZTt9abw&format=png"
+                alt=""
+              />`
+                  : ``
+              }
+            </p>
+            <p class="text-sm text-gray-400">${video.others.views} views</p>
+          </div>
+
+        </div>
+        <button onclick=loadVideoDetails('${
+          video.video_id
+        }') class="btn btn-block">Show Details</button>
+      </div>
+    
+    `;
+    videoContainer.append(videoCard);
+  });
+  hideLoader();
+};
+
+document.getElementById("search-input").addEventListener("keyup", (e) => {
+  const input = e.target.value;
+  loadVideos(input);
+});
+
+loadCategories();
